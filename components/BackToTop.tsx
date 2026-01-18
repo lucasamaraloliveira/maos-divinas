f0519ca582343dede2df4fdd6ac6f244daf79b78
Lucas Amaral Oliveira da Silva <37918350+lucasamaraloliveira@users.noreply.github.com>
2026-01-18 16:12:06 -0300

    ajustado o botão de voltar ao inicio
    ajustado o botão de voltar ao inicio

--- components/BackToTop.tsx
+++ components/BackToTop.tsx
@@ -19,7 +19,7 @@ const BackToTop: React.FC = () => {
   return (
     <button
       onClick={scrollToTop}
-      className={`fixed bottom-24 right-8 md:bottom-28 md:right-10 z-[35] bg-white/90 backdrop-blur-xl shadow-2xl w-12 h-12 md:w-14 md:h-14 flex items-center justify-center rounded-full text-[#333333] hover:bg-[#db9ea6] hover:text-white active:scale-90 border border-black/5 transition-all duration-700 ${visible ? 'translate-y-0 opacity-100' : 'translate-y-20 opacity-0 pointer-events-none'}`}
+      className={`fixed bottom-24 right-8 md:bottom-28 md:right-10 z-[35] bg-white/90 backdrop-blur-xl shadow-2xl w-12 h-12 md:w-14 md:h-14 flex items-center justify-center rounded-full text-[#333333] hover:bg-[#ed4b82] hover:text-white active:scale-90 border border-black/5 transition-all duration-700 ${visible ? 'translate-y-0 opacity-100' : 'translate-y-20 opacity-0 pointer-events-none'}`}
       aria-label="Voltar ao início"
     >
       <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
